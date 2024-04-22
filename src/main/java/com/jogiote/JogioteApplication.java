b4f5f2183b3622130a517d53e8f15fff2b444fee
kmss6905 <kmss69052@naver.com>
2024-04-22 21:58:16 +0900

    프로젝트 초기 세팅

---
+++ src/main/java/com/jogiote/JogioteApplication.java
@@ -0,0 +1,13 @@
+package com.jogiote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JogioteApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(JogioteApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/jogiote/JogioteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jogiote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JogioteApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
